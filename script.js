fe7b07ae3514dd5cdb30f7ea7fe4e9fdb281dbdf
dnyanesh <borsednyanesh99@gmail.com>
2024-08-13 01:43:03 +0530

    Billion

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleNav() {
+    const nav = document.querySelector('.nav');
+    if (nav.style.display === 'block') {
+        nav.style.display = 'none';
+    } else {
+        nav.style.display = 'block';
+    }
+}
